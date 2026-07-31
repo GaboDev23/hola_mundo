220fc77ffe400071efaa68a234cf9f0d296c03ee
Gabriel <gabo.gh12398@gmail.com>
2026-07-31 18:55:24 -0300

    Hola Mindo V4

--- script/script.js
+++ script/script.js
@@ -1,9 +1,23 @@
 const boton = document.getElementById("boton");
 const header = document.getElementById("header");
+const contador = document.querySelector(".contador");
+const mensaje = document.querySelector(".mensaje");
+
+let contClicks = 0;
 
 boton.addEventListener("click", () => {
-    alert("¡Bienvenido al mundo del desarrollo web!");
     header.textContent = "¡Gracias por visitar mi primera página!";
-    boton.disabled = true;
-    boton.textContent = "¡Gracias!";
+
+    contClicks++;
+
+    const texto = contClicks === 1 ? "vez" : "veces";
+
+    contador.textContent = `${contClicks} ${texto}`;
+
+    mensaje.textContent = "¡Gracias por visitar mi página!";
+
+    if (contClicks === 1) {
+        header.classList.add("active");
+        mensaje.classList.add("visible");
+    }
 });
